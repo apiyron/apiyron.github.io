b0c6ccfcde91bb00e28229994ee70368fef25902
Andrey <apiyron@users.noreply.github.com>
2015-10-28 19:57:17 +0300

    Update main.js

--- Audio Recorder_files/main.js
+++ Audio Recorder_files/main.js
@@ -37,7 +37,7 @@ var started = false;
 // Recogniser doesn't stop listening even if the user pauses
 recognizer.continuous = true;
 
-recognizer.lang = "en-GB";
+recognizer.lang = "ru-RU";
 
 recognizer.onerror = function(event) {
   alert('Recognition error: ' + event.message);
